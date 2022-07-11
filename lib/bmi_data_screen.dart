9bf4cfd79014ebb37f74578100af34d5310094f4
3irham <ikmuttaqin@gmail.com>
2022-07-11 11:28:39 +0700

    Base Structure change

---
+++ lib/bmi_data_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BmiDataScreen extends StatefulWidget {
+  const BmiDataScreen({super.key});
+
+  @override
+  State<BmiDataScreen> createState() => _BmiDataScreenState();
+}
+
+class _BmiDataScreenState extends State<BmiDataScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/bmi_result_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class BmiResultScreen extends StatelessWidget {
+  const BmiResultScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
