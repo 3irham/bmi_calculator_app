3799d6ee70d4e1c03f50ecc9ed05219fef478d29
3irham <ikmuttaqin@gmail.com>
2022-07-12 20:17:32 +0700

    Part 13 Update

--- lib/views/bmi_result_screen.dart
+++ lib/views/bmi_result_screen.dart
@@ -1,4 +1,5 @@
 import 'package:bmi_calculator_app/constants/constant.dart';
+import 'package:bmi_calculator_app/views/bmi_data_screen.dart';
 import 'package:flutter/material.dart';
 
 class BmiResultScreen extends StatelessWidget {
@@ -10,6 +11,79 @@ class BmiResultScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('BMI Result'),
         centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          const Expanded(
+            child: Center(
+              child: Text(
+                'Hasil Perhitungan BMI',
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: BMICard(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 80,
+                ),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: const [
+                      Text(
+                        'Normal',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        '18.5',
+                        style: TextStyle(
+                          fontSize: 95,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        'Keep it up',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ]),
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Container(
+              height: 80,
+              color: Colors.red,
+              child: const Center(
+                child: Text(
+                  'Hitung Ulang',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 23,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
